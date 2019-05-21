350b136e3673eb00502072ffd22edd8988214060
tearshred <vmiskeljin@gmail.com>
2019-05-20 19:37:20 -0700

    add init

---
+++ precache-manifest.fa6e6006db3d0146b4fe45cbce12b146.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d7b4d04131dc1b690aff48fd536a47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "638245feea84d69ba209",
+    "url": "/static/css/main.8b8a8a6e.chunk.css"
+  },
+  {
+    "revision": "746d9102ac3f331557de",
+    "url": "/static/js/2.e46a40f1.chunk.js"
+  },
+  {
+    "revision": "638245feea84d69ba209",
+    "url": "/static/js/main.8fe7703a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19f3c00d6d5fc0c620881d03ecee8e01",
+    "url": "/static/media/SUV.19f3c00d.png"
+  },
+  {
+    "revision": "05789ed27742cc94ae7825d377a736c5",
+    "url": "/static/media/Sedan.05789ed2.png"
+  },
+  {
+    "revision": "042c314aa873118d5f59a54ac85f5858",
+    "url": "/static/media/airplane-24px.042c314a.svg"
+  },
+  {
+    "revision": "36c082f416315426f5f0b31cf979b545",
+    "url": "/static/media/banner_main.36c082f4.png"
+  },
+  {
+    "revision": "42daa1a9608409b34ac798da1dbdfaa4",
+    "url": "/static/media/city-24px.42daa1a9.svg"
+  },
+  {
+    "revision": "ebcf00f4b55ed834e4c23d2d853b7538",
+    "url": "/static/media/clock-24px.ebcf00f4.svg"
+  },
+  {
+    "revision": "4dc7a8cce1559386fa04e2b84289a9d4",
+    "url": "/static/media/email-24px.4dc7a8cc.svg"
+  }
+]);
